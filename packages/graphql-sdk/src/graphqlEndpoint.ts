584c0bd75ba08daa1611522da96d4030eaddecd4
Cameron Taylor <cameron@lastrev.com>
2024-03-08 17:08:41 -0500

    Formatting

--- packages/graphql-sdk/src/graphqlEndpoint.ts
+++ packages/graphql-sdk/src/graphqlEndpoint.ts
@@ -1,4 +1,5 @@
-const deployUrl = process.env.DEPLOY_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null);
+const deployUrl =
+  process.env.DEPLOY_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null);
 
 export const graphqlEndpoint =
   process.env.NEXT_PHASE === 'phase-production-build' || process.env.STAGE === 'build' || !deployUrl
