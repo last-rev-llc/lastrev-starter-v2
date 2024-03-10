915a4589a5b36a0dedc97d839fe13d85b1b496d0
Cameron Taylor <cameron@lastrev.com>
2024-03-09 21:39:44 -0500

    Update Git Actions loader script and remove unit test workflow

--- scripts/env-checks/ci/git_actions_loader.sh
+++ scripts/env-checks/ci/git_actions_loader.sh
@@ -3,11 +3,11 @@
 # Installing envkey-source
 VERSION=$(curl https://envkey-releases.s3.amazonaws.com/latest/envkeysource-version.txt) && curl -s https://envkey-releases.s3.amazonaws.com/envkeysource/release_artifacts/$VERSION/install.sh | bash
 
-# Loading env vars from EnvKey
-es --dot-env -f -- > $GITHUB_ENV
+# # Loading env vars from EnvKey
+# es --dot-env -f -- > $GITHUB_ENV
 
-# Stripping quotes from all env vars
-while IFS='=' read -r name value; do
-  cleaned_value=$(echo "$value" | tr -d "'")
-  echo "$name=$cleaned_value" >> $GITHUB_ENV
-done < <(env)
+# # Stripping quotes from all env vars
+# while IFS='=' read -r name value; do
+#   cleaned_value=$(echo "$value" | tr -d "'")
+#   echo "$name=$cleaned_value" >> $GITHUB_ENV
+# done < <(env)
