851acc1b508e4c6e26884ebdd9640bd3ceb41a3f
Cameron Taylor <cameron@lastrev.com>
2024-03-10 12:11:34 -0400

    Remove code for stripping quotes from environment variables

--- scripts/env-checks/ci/git_actions_loader.sh
+++ scripts/env-checks/ci/git_actions_loader.sh
@@ -6,8 +6,8 @@ VERSION=$(curl https://envkey-releases.s3.amazonaws.com/latest/envkeysource-vers
 # Loading env vars from EnvKey
 es --dot-env -f -- > $GITHUB_ENV
 
-# Stripping quotes from all env vars
-while IFS='=' read -r name value; do
-  cleaned_value=$(echo "$value" | tr -d "'")
-  echo "$name=$cleaned_value" >> $GITHUB_ENV
-done < <(env)
+# # Stripping quotes from all env vars
+# while IFS='=' read -r name value; do
+#   cleaned_value=$(echo "$value" | tr -d "'")
+#   echo "$name=$cleaned_value" >> $GITHUB_ENV
+# done < <(env)
