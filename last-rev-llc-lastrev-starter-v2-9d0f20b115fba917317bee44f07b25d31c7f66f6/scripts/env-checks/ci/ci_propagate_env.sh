2640c0f4179a21afd708c71bea156679b893c746
Jaime Morales <jaime@lastrev.com>
2024-04-22 16:38:06 +0200

    Initial commit

---
+++ last-rev-llc-lastrev-starter-v2-9d0f20b115fba917317bee44f07b25d31c7f66f6/scripts/env-checks/ci/ci_propagate_env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# ANSI Color Codes
+RED='\033[0;31m'     # Red for errors
+GREEN='\033[0;32m'   # Green for success messages
+CYAN='\033[0;36m'    # Cyan for informational messages
+NC='\033[0m'         # No Color, to reset to the default terminal color scheme
+
+# Path to the root .env file
+root_env_file=".env"
+
+# Path to the packages directory and the specific 'web' directory
+packages_dir="packages"
+web_dir="apps/web"
+
+# Check if .env file exists
+if [ ! -f "$root_env_file" ]; then
+    echo -e "${RED}Root .env file not found.${NC}"
+    exit 1
+fi
+
+# Copy to the 'web' directory
+if [ -d "$web_dir" ]; then
+    cp "$root_env_file" "$web_dir/.env"
+    echo -e "${CYAN}Copied .env file to ${GREEN}$web_dir${NC}"
+fi
+
+# Iterate over subdirectories in the packages directory and copy to their roots
+for package in "$packages_dir"/*; do
+    if [ -d "$package" ]; then
+        # Copy the .env file to the root of each package
+        cp "$root_env_file" "$package/.env"
+        echo -e "${CYAN}Copied .env file to the root of ${GREEN}$package${NC}"
+    fi
+done
+
+echo -e "${GREEN}Successfully copied .env file to the web directory and all package roots.${NC}"
