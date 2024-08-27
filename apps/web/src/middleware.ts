b9bb5afee2de777a501a5879f949ecb460925a91
Cameron Taylor <cameron@lastrev.com>
2024-08-27 15:52:51 -0400

    Fixing CSP refactor issues

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,17 +2,17 @@ import { NextResponse } from 'next/server';
 
 export function middleware() {
   const cspHeader = `
-    default-src 'self' *.flowise.theanswer.ai *.theanswer.ai localhost:8888 *.sentry.io *.facebook.com vitals.vercel-insights.com *.hubapi.com *.hsforms.com *.hs-scripts.com *.hsforms.net *.hscollectedforms.net *.netlify.app *.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
-    style-src 'self' 'unsafe-inline' *.sentry.io fonts.googleapis.com vitals.vercel-insights.com *.hs-scripts.com *.hsforms.net *.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' *.flowise.theanswer.ai *.theanswer.ai *.sentry.io *.google-analytics.com *.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com vitals.vercel-insights.com *.hs-analytics.net *.hs-banner.com *.hsadspixel.net *.hscollectedforms.net *.jquery.com *.hs-scripts.com *.hsforms.net netlify-cdp-loader.netlify.app;
-    font-src 'self' *.sentry.io fonts.gstatic.com *.hs-scripts.com *.hsforms.net data: *.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
-    frame-src 'self' lastrev.com forms.hsforms.com *.theanswer.ai *.flowise.theanswer.ai *.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
-    img-src * data: *.googletagmanager.com https://*.ctfassets.net https://*.googletagmanager.com https://*.google-analytics.com;
+    default-src 'self' https://*.flowise.theanswer.ai https://*.theanswer.ai localhost:8888 https://*.sentry.io https://*.facebook.com vitals.vercel-insights.com https://*.hubapi.com https://*.hsforms.com https://*.hs-scripts.com https://*.hsforms.net https://*.hscollectedforms.net https://*.netlify.app https://*.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
+    style-src 'self' 'unsafe-inline' https://*.sentry.io fonts.googleapis.com vitals.vercel-insights.com https://*.hs-scripts.com https://*.hsforms.net https://*.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.flowise.theanswer.ai https://*.theanswer.ai https://*.sentry.io https://*.google-analytics.com https://*.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com vitals.vercel-insights.com https://*.hs-analytics.net https://*.hs-banner.com https://*.hsadspixel.net https://*.hscollectedforms.net https://*.jquery.com https://*.hs-scripts.com https://*.hsforms.net netlify-cdp-loader.netlify.app;
+    font-src 'self' https://*.sentry.io fonts.gstatic.com https://*.hs-scripts.com https://*.hsforms.net data: https://*.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
+    frame-src 'self' lastrev.com forms.hsforms.com https://*.theanswer.ai https://*.flowise.theanswer.ai https://*.googletagmanager.com https://*.googletagmanager.com https://*.google-analytics.com;
+    img-src * data: https://*.googletagmanager.com https://*.ctfassets.net https://*.googletagmanager.com https://*.google-analytics.com;
     media-src * data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors 'self' https://app.contentful.com https://lastrev.com https://lr-live-editor.netlify.app *.theanswer.ai *.flowise.theanswer.ai;
+    frame-ancestors 'self' https://app.contentful.com https://lastrev.com https://lr-live-editor.netlify.app https://*.theanswer.ai https://*.flowise.theanswer.ai;
     block-all-mixed-content;
     upgrade-insecure-requests;
   `
