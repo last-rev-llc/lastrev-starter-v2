16a7f8a4ecd9bc6e31d781cef9bd33b23c15dbf4
Jaime Morales <jaime@lastrev.com>
2025-06-12 18:42:34 +0200

    sitemap updates

--- packages/graphql-extensions/src/Site.extension.ts
+++ packages/graphql-extensions/src/Site.extension.ts
@@ -1,9 +1,3 @@
-import type { Entry } from '@last-rev/types';
-
-import { getLocalizedField } from '@last-rev/graphql-cms-core';
-
-import { pathNodeResolver } from './utils/pathNodeResolver';
-
 import type { ApolloContext } from './types';
 import gql from 'graphql-tag';
 
@@ -50,72 +44,6 @@ export const resolvers = {
       ctx.siteSettings = siteSettings;
 
       return pathEntries.reduce((acc: any, curr: any) => (curr ? curr : acc), null as any);
-    },
-    sitemapPage: async (
-      _: never,
-      {
-        contentType,
-        locale,
-        preview = false,
-        site,
-        page
-      }: {
-        contentType: string;
-        locale: string;
-        preview?: boolean;
-        site: string;
-        page: number;
-      },
-      ctx: ApolloContext
-    ) => {
-      ctx.preview = !!preview;
-
-      const maxPageSize = 100; //config.sitemap.maxPageSize;
-
-      const buildSitemapPath = (path: string) =>
-        `${locale === ctx.defaultLocale ? '' : `${locale}/`}${path.replace(/^\//, '')}`;
-
-      const ids = (
-        await ctx.contentful![preview ? 'preview' : 'prod'].getEntries({
-          content_type: contentType,
-          select: `sys.id`,
-          limit: maxPageSize,
-          skip: (page - 1) * maxPageSize,
-          order: 'sys.updatedAt'
-        })
-      ).items.map((item: Entry<any>) => item.sys.id);
-
-      const entries = (
-        await ctx.loaders.entryLoader.loadMany(ids.map((id: string) => ({ id, preview })))
-      ).filter((e: any) => !!e && !(e instanceof Error)) as Entry<any>[];
-
-      const sitemapEntries = (
-        await Promise.all(
-          entries.map(async (entry) => {
-            const seo = getLocalizedField(entry.fields, 'seo', ctx);
-            if (seo?.['robots']?.value?.startsWith('noindex')) {
-              return [];
-            }
-
-            const pathNode = await pathNodeResolver(entry.sys.id, ctx);
-
-            if (pathNode?.data?.excludedLocales?.includes(locale)) {
-              return [];
-            }
-
-            const paths = await ctx.loadPathsForContent(entry, ctx, site);
-
-            return paths.map((p: any) => ({
-              loc: buildSitemapPath(p.path),
-              lastmod: entry.sys.updatedAt
-            }));
-          })
-        )
-      ).flat();
-
-      return {
-        entries: sitemapEntries
-      };
     }
   }
 };
